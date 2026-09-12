b7df8c45440c34f06dd1eff39b647b32ca2588f3
Shobhit Gupta <shobhitgupat8398@gmail.com>
2026-09-12 15:44:00 +0530

    Sync Print 1 to N using Recursion [DSA] - TUFHub

--- DSA/Recursion/print-1-to-n-using-recursion/solution.py
+++ DSA/Recursion/print-1-to-n-using-recursion/solution.py
@@ -1,11 +1,12 @@
 class Solution:
+    def helperfunc(self,i,n):
+        if (i>n):
+           return
+        else:
+            print(i)
 
-    def helper(self,current,n):
-        if current >n:
-            return
-        print(current)
-        self.helper(current+1,n)    
+        self.helperfunc(i+1,n)    
+
+    def printNumbers(self, n):
+        self.helperfunc(1,n)
         
-    def printNumbers(self,n):
-        self.helper(1,n)
-    
